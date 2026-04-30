ed67d44a0d3387016efff8599537c42d1d416dc0
Joseph Chavez <chavezaurus@gmail.com>
2026-04-30 13:13:45 -0600

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir logs
+
+## do a quick update
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y;
+
+## install python essentials
+sudo apt-get install -y tmux;
+
+curl -LsSf https://astral.sh/uv/install.sh | sh;
